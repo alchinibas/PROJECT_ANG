fe02ef87b8f336effb90a1d4bdd54cf7785c6064
alchinibas <alchinibas123@gmail.com>
2021-08-07 17:17:24 +0545

    removes the video stream sample v0

--- app.py
+++ app.py
@@ -83,27 +83,6 @@ def register():
     return redirect('../')
 
 
-camera=cv2.VideoCapture(0)
-
-def get_frames():
-    while True:
-        success,frame=camera.read()
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-            yield(
-                b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
-            )
-
-@app.route('/video')
-def video():
-    return render_template('fv/videotest.html')
-@app.route('/getvideo')
-def get_video():
-    return Response(get_frames(), mimetype="multipart/x-mixed-replace;boundary=frame")
 
 
 ###api
